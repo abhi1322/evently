03ac9c7a6a7fde15bf7d30193f41c5ac523654a5
abhi1322 <abhi1322jii@gmail.com>
2024-01-14 13:06:43 +0530

    stripe- implement

---
+++ lib/uploadthings.ts
@@ -0,0 +1,4 @@
+import { OurFileRouter } from "@/app/api/webhook/clerk/uploadthing/core";
+import { generateReactHelpers } from "@uploadthing/react/hooks";
+
+export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
